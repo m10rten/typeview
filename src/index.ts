90454487931c02cd130d61fb8e8fd5e41341cba5
m10rten <qmaartenenzo@gmail.com>
2025-09-14 23:06:46 +0200

    update index.ts & .github workflows

--- src/index.ts
+++ src/index.ts
@@ -4,3 +4,12 @@ export const version = {
   patch: 0,
 };
 export const name = "typeview";
+export class TypeView {
+  public constructor() {
+    throw new TypeError(
+      `Oh no! Version ${version.major}.${version.minor}.${version.patch} of 'typeview' does not contain any functionality.`,
+    );
+  }
+}
+
+export default TypeView;
